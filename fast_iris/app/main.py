ad4352eca818dad68dd57818a52467bd848ab06d
jia <jiasungdia@gmail.com>
2024-09-10 15:32:50 +0900

    fastapi_nginx_gr

--- fast_iris/app/main.py
+++ fast_iris/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, Form
+from starlette.responses import Response
+from prometheus_client import Counter, generate_latest
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -10,18 +12,25 @@ from fastapi import Request
 
 # FastAPI 인스턴스 생성
 app = FastAPI()
-
+REQUEST_COUNT = Counter("request_count", "Total Request Count", ["method", "endpoint"])
 # Jinja2 템플릿 설정
 templates = Jinja2Templates(directory="../app/templates")
 
 
 model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'model', 'iris_m_ver_20240908_2306.pkl')
-
-
 model = joblib.load(model_path)
 
 # Iris 데이터셋 라벨 이름
 label_names = ['setosa', 'versicolor', 'virginica']
+
+# 메트릭 수집 미들웨어 추가
+@app.middleware("http")
+async def prometheus_middleware(request: Request, call_next):
+    method = request.method
+    endpoint = request.url.path
+    response = await call_next(request)
+    REQUEST_COUNT.labels(method=method, endpoint=endpoint).inc()  # 요청 카운터 증가
+    return response
 
 # 메인 페이지 경로
 @app.get("/", response_class=HTMLResponse)
@@ -41,6 +50,11 @@ async def predict(request: Request, sepal_length: float = Form(...), sepal_width
     
     # 결과 반환 (HTML 페이지에 예측 결과 전달)
     return templates.TemplateResponse("index.html", {"request": request, "prediction": prediction_label})
+
+# Prometheus 메트릭 경로 추가
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
 
 # FastAPI 실행 부분 (uvicorn으로 실행해야 함)
 if __name__ == "__main__":
